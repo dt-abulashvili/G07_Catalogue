3129aaa9688da347dbd8c143e7d88ce5c78cfc3e
dt-abulashvili <darkusexidra@gmail.com>
2025-06-29 10:12:14 +0400

    Add transaction management to DatabaseWriter

--- Catalogue.Database.Importer/DatabaseWriter.cs
+++ Catalogue.Database.Importer/DatabaseWriter.cs
@@ -2,6 +2,7 @@
 using Catalogue.Shared.Models;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Transactions;
 
 namespace Catalogue.Database.Importer;
 
@@ -18,83 +19,91 @@ internal class DatabaseWriter : IDataWriter<Category>
     {
         ArgumentNullException.ThrowIfNull(data);
 
-        using (SqlConnection connection = new SqlConnection(_connectionString))
-        {
-            connection.Open();
+        using SqlConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
 
+        using SqlTransaction transaction = connection.BeginTransaction();
+        try
+        {
             foreach (var category in data)
             {
-                int? categoryId = GetCategoryIdByName(category.Name, connection);
+                int? categoryId = GetCategoryIdByName(category.Name, connection, transaction);
 
                 if (categoryId == null)
                 {
-                    categoryId = InsertCategory(category, connection);
+                    categoryId = InsertCategory(category, connection, transaction);
                 }
-                else if (IsCategoryChanged(categoryId.Value, category, connection))
+                else if (IsCategoryChanged(categoryId.Value, category, connection, transaction))
                 {
-                    UpdateCategory(categoryId.Value, category, connection);
+                    UpdateCategory(categoryId.Value, category, connection, transaction);
                 }
 
                 foreach (var product in category.Products.Values)
                 {
-                    int? productId = GetProductIdByName(product.Code, connection);
+                    int? productId = GetProductIdByName(product.Code, connection, transaction);
 
                     if (productId == null)
                     {
-                        InsertProduct(product, categoryId.Value, connection);
+                        InsertProduct(product, categoryId.Value, connection, transaction);
                     }
-                    else if (IsProductChanged(product.Code, product, connection))
+                    else if (IsProductChanged(product.Code, product, connection, transaction))
                     {
-                        UpdateProduct(product, connection);
+                        UpdateProduct(product, connection, transaction);
                     }
                 }
             }
+            transaction.Commit();
+        }
+        catch
+        {
+            transaction.Rollback();
+            throw;
         }
     }
 
-    private int? GetCategoryIdByName(string categoryName, SqlConnection connection)
+    private int? GetCategoryIdByName(string categoryName, SqlConnection connection, SqlTransaction transaction)
     {
         using SqlCommand command = new SqlCommand(
-            "select CategoryID from Categories where CategoryName = @Name", connection);
+            "select CategoryID from Categories where CategoryName = @Name", connection, transaction);
         command.Parameters.AddWithValue("@Name", categoryName);
         object? result = command.ExecuteScalar();
 
         return result != null ? Convert.ToInt32(result) : null;
     }
 
-    private int? GetProductIdByName(string productCode, SqlConnection connection)
+    private int? GetProductIdByName(string productCode, SqlConnection connection, SqlTransaction transaction)
     {
         using SqlCommand command = new SqlCommand(
-            "select ProductID from Products where ProductCode = @Code", connection);
+            "select ProductID from Products where ProductCode = @Code", connection, transaction);
         command.Parameters.AddWithValue("@Code", productCode);
         object? result = command.ExecuteScalar();
 
         return result != null ? Convert.ToInt32(result) : null;
     }
 
-    private int InsertCategory(Category category, SqlConnection connection)
+    private int InsertCategory(Category category, SqlConnection connection, SqlTransaction transaction)
     {
-        using SqlCommand cmd = new SqlCommand("InsertCategory", connection);
+        using SqlCommand cmd = new SqlCommand("InsertCategory", connection, transaction);
         cmd.CommandType = CommandType.StoredProcedure;
         cmd.Parameters.AddWithValue("@CategoryName", category.Name);
         cmd.Parameters.AddWithValue("@IsActive", category.IsActive);
         cmd.ExecuteNonQuery();
 
-        return GetCategoryIdByName(category.Name, connection) ?? throw new Exception("Failed to retrieve inserted category ID.");
+        return GetCategoryIdByName(category.Name, connection, transaction) ?? throw new Exception("Failed to retrieve inserted category ID.");
     }
 
-    private void UpdateCategory(int categoryId, Category category, SqlConnection connection)
+    private void UpdateCategory(int categoryId, Category category, SqlConnection connection, SqlTransaction transaction)
     {
-        using SqlCommand cmd = new SqlCommand("UpdateCategory", connection);
+        using SqlCommand cmd = new SqlCommand("UpdateCategory", connection, transaction);
         cmd.CommandType = CommandType.StoredProcedure;
         cmd.Parameters.AddWithValue("@CategoryID", categoryId);
         cmd.Parameters.AddWithValue("@IsActive", category.IsActive);
         cmd.ExecuteNonQuery();
     }
 
-    private void InsertProduct(Product product, int categoryId, SqlConnection connection)
+    private void InsertProduct(Product product, int categoryId, SqlConnection connection, SqlTransaction transaction)
     {
-        using SqlCommand cmd = new SqlCommand("InsertProduct", connection);
+        using SqlCommand cmd = new SqlCommand("InsertProduct", connection, transaction);
         cmd.CommandType = CommandType.StoredProcedure;
         cmd.Parameters.AddWithValue("@CategoryID", categoryId);
         cmd.Parameters.AddWithValue("@ProductCode", product.Code);
@@ -104,9 +113,9 @@ internal class DatabaseWriter : IDataWriter<Category>
         cmd.ExecuteNonQuery();
     }
 
-    private void UpdateProduct(Product product, SqlConnection connection)
+    private void UpdateProduct(Product product, SqlConnection connection, SqlTransaction transaction)
     {
-        using SqlCommand cmd = new SqlCommand("UpdateProduct", connection);
+        using SqlCommand cmd = new SqlCommand("UpdateProduct", connection, transaction);
         cmd.CommandType = CommandType.StoredProcedure;
         cmd.Parameters.AddWithValue("@ProductCode", product.Code);
         cmd.Parameters.AddWithValue("@ProductName", product.Name);
@@ -115,10 +124,10 @@ internal class DatabaseWriter : IDataWriter<Category>
         cmd.ExecuteNonQuery();
     }
 
-    private bool IsCategoryChanged(int categoryId, Category category, SqlConnection connection)
+    private bool IsCategoryChanged(int categoryId, Category category, SqlConnection connection, SqlTransaction transaction)
     {
         using SqlCommand command = new SqlCommand(
-            "select IsActive from Categories where CategoryID = @ID", connection);
+            "select IsActive from Categories where CategoryID = @ID", connection, transaction);
         command.Parameters.AddWithValue("@ID", categoryId);
 
         using SqlDataReader reader = command.ExecuteReader();
@@ -131,10 +140,10 @@ internal class DatabaseWriter : IDataWriter<Category>
         return true; //issue here
     }
 
-    private bool IsProductChanged(string productCode, Product product, SqlConnection connection)
+    private bool IsProductChanged(string productCode, Product product, SqlConnection connection, SqlTransaction transaction)
     {
         using SqlCommand command = new SqlCommand(
-            "select ProductName, Price, IsActive from Products where ProductCode = @Code", connection);
+            "select ProductName, Price, IsActive from Products where ProductCode = @Code", connection, transaction);
         command.Parameters.AddWithValue("@Code", productCode);
 
         using SqlDataReader reader = command.ExecuteReader();
